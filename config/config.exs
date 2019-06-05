ec9c4c5b4609d24902406675d00f7062e8f642bc
Peter Oas <peter.oas@revelry.co>
2019-06-05 14:09:59 -0500

    Non functional OAuth system

--- config/config.exs
+++ config/config.exs
@@ -28,3 +28,12 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "e720ff8629551ac17fb0",
+  client_secret: "666e9bb14092afd3aad0a446140369e65d70d432"
